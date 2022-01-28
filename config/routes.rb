1901a123b4e61b1f499b327626cd06a6b12a204d
Eduardo Lopez <jramos@arkusnexus.com>
2022-01-28 10:58:33 -0800

    creating preliminar ui flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   root "dashboard#index"
-  devise_for :customers
   devise_for :users
 
-  scope "/customers" do
-    resources :pets, path: "/pets"
+  resources :users, path: "/users", only: [] do
+    resources :shelters, path: "/shelters" do
+      resources :pets, path: "/pets"
+    end
+
+    resources :rescuers, path: "/rescuers" do
+      resources :pets, path: "/pets"
+    end
   end
 end
